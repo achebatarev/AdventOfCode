b168283dc426ef1beed7dd24e77564ac24c75cb9
Alexandr Chebatarev <alexandr.chebatarev@gmail.com>
2024-12-12 13:38:09 -0500

    Add p2

--- 2024/04p/main.py
+++ 2024/04p/main.py
@@ -1,4 +1,3 @@
-letters = {'X': 'M', 'M': 'A', "A": "S"}
 mapping = ((0, 1), (1, 0), (-1, 0), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1))
 def dfs(arr, y, x, count):
     res = 0
@@ -6,23 +5,31 @@ def dfs(arr, y, x, count):
         yr = y
         xc = x
         l = []
-        locs = []
         for _ in range(4):
             if 0 <= yr < len(arr) and 0 <= xc < len(arr[0]):
                 l.append(arr[yr][xc])
-                # locs.append((yr, xc))
-            # because of this lazy handling, some values become negative and wrap around
             yr = yr+r
             xc = xc+c
         if ''.join(l) == 'XMAS':
             res += 1
-            # print(count, locs, l)
             count+=1
     return res, count
 
+def dfs2(arr, y, x):
+    # I need to check these relative locations and make sure that it has 'M', 'S' present
+    left = ((1, 1), (-1, -1))
+    right = ((1, -1), (-1, 1))
+    ll = []
+    lr = []
+    for r, c in left:
+        if 0 <= y+r < len(arr) and 0 <= x+c < len(arr[0]):
+            ll.append(arr[y+r][x+c])
+    for r, c in right:
+        if 0 <= y+r < len(arr) and 0 <= x+c < len(arr[0]):
+            lr.append(arr[y+r][x+c])
+    print(sorted(ll), sorted(lr), int(True))
+    return int(sorted(ll)== sorted(lr) and sorted(ll) == ['M', 'S']) 
 
-
-# f = open('input.d4')
 f = open('input.d4')
 arr = f.read().splitlines()
 ans = 0
@@ -32,5 +39,17 @@ for y, line in enumerate(arr):
         if e == 'X':
             a, c = dfs(arr, y, x, c)
             ans += a
-
 print(ans)
+
+
+f = open('input.d4')
+arr = f.read().splitlines()
+ans = 0
+c = 0
+for y, line in enumerate(arr):
+    for x, e in enumerate(line):
+        if e == 'A':
+            ans += dfs2(arr, y, x)
+print(ans)
+
+
