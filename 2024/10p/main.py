a3e87a92a8f1b7421d74ab6a73e0e29443efe987
Alexandr Chebatarev <alexandr.chebatarev@gmail.com>
2025-01-07 19:08:20 -0500

    Replace with gaussian coords solution

--- 2024/10p/main.py
+++ 2024/10p/main.py
@@ -5,32 +5,21 @@ try:
 except:
     FILE = 'test.in'
 
+
 grid = list(map(list, open(FILE).read().splitlines()))
 grid = [list(map(int, line)) for line in grid]
+G = {y+x*1j: c
+     for y, l in enumerate(open(FILE).read().splitlines())
+     for x, c in enumerate(map(int, l))}
 mapping = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
-def dfs(grid, y, x, prev, visited):
-    # print(len(grid), len(grid[0]))
-    # print(y, x)
-    if 0 <= y < len(grid) and 0 <= x < len(grid[0]) and (grid[y][x] - prev == 1):
-        if grid[y][x] == 9:
-            # if (y, x) in visited:
-            #     return 0
-            # visited.add((y, x))
-            return 1
-        a = 0
-        for r, c in mapping:
-            yr = y+r
-            xc = x+c
-            a += dfs(grid, yr, xc, grid[y][x], visited)
-        return a
+def gaussian_dfs(grid, p, prev):
+    if p not in G or grid[p] - prev != 1:
+        return 0
+    if grid[p] == 9:
+        return 1
+    return sum(gaussian_dfs(grid, p+r+c*1j, grid[p]) for r, c in mapping)
 
-    return 0
-
-ans = 0
-for i, line in enumerate(grid):
-    for j, e in enumerate(line):
-        if e == 0:
-            visited = set()
-            ans += dfs(grid, i, j, -1, visited)
-print('PART 1:', ans)
+ans = sum(gaussian_dfs(G, k, -1) for k in G)
+        
+print('PART 2:', ans)
