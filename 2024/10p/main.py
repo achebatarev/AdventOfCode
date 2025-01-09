989625bf50c86adb34b68bf316722fb34c0db675
Alexandr Chebatarev <alexandr.chebatarev@gmail.com>
2025-01-09 17:18:22 -0500

    Rewrite with complex numbers

--- 2024/10p/main.py
+++ 2024/10p/main.py
@@ -5,21 +5,19 @@ try:
 except:
     FILE = 'test.in'
 
+G = { y+x*1j: int(e)
+        for y, line in enumerate(open(FILE).read().splitlines())
+        for x, e in enumerate(line)
+    }
 
-grid = list(map(list, open(FILE).read().splitlines()))
-grid = [list(map(int, line)) for line in grid]
-G = {y+x*1j: c
-     for y, l in enumerate(open(FILE).read().splitlines())
-     for x, c in enumerate(map(int, l))}
-mapping = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-
-def gaussian_dfs(grid, p, prev):
-    if p not in G or grid[p] - prev != 1:
-        return 0
-    if grid[p] == 9:
+def dfs(g, p):
+    if g[p] == 9:
         return 1
-    return sum(gaussian_dfs(grid, p+r+c*1j, grid[p]) for r, c in mapping)
+    ans = 0
+    for e in (p+d for d in [1, -1, 1j, -1j] if g.get(p+d) == g[p] + 1):
+        ans += dfs(g, e)
+    return ans
 
-ans = sum(gaussian_dfs(G, k, -1) for k in G)
+ans = sum(dfs(G, p) for p in G if G[p] == 0)
         
 print('PART 2:', ans)
